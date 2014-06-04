32f89bec40d44e31c5c57eb5ce71360e5d7c0932
Jared Reynolds <jreynolds@daptiv.com>
2014-06-04 15:54:06 -0700

    Used _.defaults to cleanup applyTemplateData

--- src/lib/applyTemplateData.js
+++ src/lib/applyTemplateData.js
@@ -6,13 +6,12 @@ var _ = require('underscore'),
 function applyTemplateData(template, data) {
     var match,
         templateText,
-        dataSuperset = {};
+        dataSuperset = _.clone(data),
+        templateFieldDefaults = {};
 
     if (!data) {
         return template;
     }
-
-    var originalDataKeys = _.keys(data);
 
     if (_.isObject(template)) {
         templateText = JSON.stringify(template);
@@ -21,13 +20,10 @@ function applyTemplateData(template, data) {
     while (match = fieldFinder.exec(templateText))
     {
         var field = match[1];
-        var keyExistsInData = _.contains(originalDataKeys, field);
-        if (keyExistsInData) {
-            dataSuperset[field] = data[field] || '';
-        } else {
-            dataSuperset[field] = '{' + field + '}';
-        }
+        templateFieldDefaults[field] = '{' + field + '}';
     }
+
+    _.defaults(dataSuperset, templateFieldDefaults);
 
     var templatedText = _.template(templateText, dataSuperset, { interpolate: fieldFinder });
     return JSON.parse(templatedText);
